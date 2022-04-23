f2a3c83e995f0540c15211fe1236daf97ffd2895
speed up <orlenkoo355@gmail.com>
2022-04-23 22:11:58 +0900

    update

---
+++ publish/wwwroot/js/project.js
@@ -0,0 +1,31 @@
+﻿$(() => {
+    $("#btn-copy-select").click(function () {
+        window.location.href = "/admin/project/add?projectIdToCopy=" + getCheckedProject();
+    });
+
+    $("#btn-archive-select").click(function () {
+        window.location.href = "/admin/project/" + getCheckedProject() + "/archive";
+    });
+
+    $("#btn-unarchive-select").click(function () {
+        window.location.href = "/admin/project/" + getCheckedProject() + "/restore";
+    });
+
+    $("#btn-delete-select").click(function () {
+        window.location.href = "/admin/project/" + getCheckedProject() + "/delete";
+    });
+});
+
+function getCheckedProject() {
+    var projectIds = "";
+    $(".checkbox-project").each(function () {
+        if ($(this).prop("checked") == true) {
+            if (projectIds == "") {
+                projectIds += $(this).attr("projectid");
+            } else {
+                projectIds = projectIds + "," + $(this).attr("projectid");
+            }
+        }
+    })
+    return projectIds;
+}
